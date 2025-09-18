3638f08b5b9d4b14a7b9e917a9fd44bc5e48a206
Urs <ora8@gmx.ch>
2025-09-18 08:50:16 +0200

    Initialization

---
+++ urswag/src/lib/components/MainSection.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+
+</script>
+<section class="main-section default-margin">
+    <h3>Hello I'm Urs</h3>
+    <div class="underscore mb-m">
+    </div>
+    <h1>I'm a Software Developer of C++, C, C#, Svelte.</h1>
+</section>
